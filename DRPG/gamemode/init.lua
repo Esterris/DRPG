6d6c0a0b9442d5bc2e9c4728e0714ae3018943df
Devery <turc0180@algonquinlive.com>
2020-02-27 17:12:33 -0500

    Inital files created.

---
+++ DRPG/gamemode/init.lua
@@ -0,0 +1,8 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+local function GM:PlayerConnect(name, ip)
+	
+end
---
+++ DRPG/gamemode/shared.lua
@@ -0,0 +1,6 @@
+GM.name = "DRPG"
+GM.Author = "Devery"
+
+function GM:Initialize()
+	self.baseclass.Initialize(self)
+end
